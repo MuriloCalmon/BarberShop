2a9a64659d13abec5beb66bb4d59f61d1086e476
Murilo Calmon <murilocalmon80@gmail.com>
2024-07-30 23:07:00 -0300

    menu burger test

--- src/pages/barber/index.tsx
+++ src/pages/barber/index.tsx
@@ -1,8 +1,58 @@
 import { Search, Star } from "lucide-react";
 import { useNavigate } from "react-router-dom";
+import { stack as Menu } from "react-burger-menu";
 
+const styles = {
+    bmBurgerButton: {
+        position: 'fixed',
+        width: '36px',
+        height: '30px',
+        right: '36px',
+        top: '36px',
+    },
+    bmBurgerBars: {
+        background: '#09090b'
+    },
+    bmBurgerBarsHover: {
+        background: '#a90000'
+    },
+    bmCrossButton: {
+        height: '24px',
+        width: '24px'
+    },
+    bmCross: {
+        background: '#fff'
+    },
+    bmMenuWrap: {
+        position: 'fixed',
+        height: '100%'
+    },
+    bmMenu: {
+        background: '#09090b',
+        padding: '2.5em 1.5em 0',
+        fontSize: '16px',
+    },
+    bmMorphShape: {
+        fill: '#373a47'
+    },
+    bmItemList: {
+        color: '#b8b7ad',
+        padding: '0.8em'
+    },
+    bmItem: {
+        display: 'block',
+        color: '#fff',
+        marginBottom: '0.8em',
+        
+    },
+    bmOverlay: {
+        background: 'rgba(0, 0, 0, 0.3)'
+    }
+}
 
 export function Barber() {
+
+
 
     const navigate = useNavigate()
 
@@ -16,18 +66,11 @@ export function Barber() {
 
         <div className="px-7">
             <div className="space-y-24">
-                <nav className="flex justify-between items-center px-3 py-1 rounded-lg mt-4">
-                    <div>
-                        <img className="size-12" src="./barber.png" alt="logo" />
-                    </div>
-                    <div>
-                        <ul className="flex gap-2 text-slate-100 cursor-pointer">
-                            <li><a className="" href="" >Início</a></li>
-                            <li><a href="">Sair</a></li>
-                        </ul>
-                    </div>
-                </nav>
-                
+
+                <Menu right styles={styles} >
+                    <a id="out" className="menu-item" href="/">Sair</a>
+                </Menu>
+
 
                 <div className="flex items-center justify-between px-4 py-2 rounded-lg bg-zinc-900">
                     <span className="text-xs text-slate-100">Barbeiros disponíveis</span>
