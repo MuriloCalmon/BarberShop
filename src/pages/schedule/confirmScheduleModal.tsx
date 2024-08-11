3f757261826d009dd179494e73fe80f27b6a17c0
Murilo Calmon <murilocalmon80@gmail.com>
2024-08-10 23:42:23 -0300

    update  page schedule

--- src/pages/schedule/confirmScheduleModal.tsx
+++ src/pages/schedule/confirmScheduleModal.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Button } from "../../components/Button"
 
 interface ConfirmScheduleModalProps {
@@ -5,6 +6,18 @@ interface ConfirmScheduleModalProps {
 }
 
 export function ConfirmScheduleModal({ closeModalSchedule }: ConfirmScheduleModalProps) {
+
+    // function alterarCor(e) {
+    //     e.target.style.backgroundColor = "#f59e0b";
+    // }
+
+    const [color, setColor] = useState('');
+
+    const handleColor = () => {
+      setColor('#f59e0b');
+    };
+
+
     return (
         <div className="fixed z-20 inset-0 bg-black/60 flex items-center justify-center">
             <div className="bg-amber-500 shadow-shape w-80  rounded-lg p-4 space-y-6">
@@ -17,12 +30,23 @@ export function ConfirmScheduleModal({ closeModalSchedule }: ConfirmScheduleModa
                     <p>R$: 18,00</p>
                 </div>
                 <div className="flex flex-col gap-1 bg-slate-100 rounded-lg items-center justify-between p-4">
-                    <p>Escolha a data</p>
+                    <p className="font-bold">Escolha a data</p>
                     <input type="date" name="" id="" className="p-2 rounded-lg border-none bg-slate-100 text-zinc-900 text-center" />
                 </div>
                 <div className="flex flex-col gap-1 bg-slate-100 rounded-lg items-center justify-between p-4">
-                    <p>Escolha o horário</p>
-                    <input type="time" name="" id="" className="p-2 rounded-lg border-none bg-slate-100 text-zinc-900 text-center" />
+                    <div className="flex flex-col items-center gap-3">
+                        <p className="font-bold">Escolha o horário</p>
+                        <div className="grid grid-cols-4 gap-3">
+                            <span onClick={handleColor} style={{backgroundColor: color}} className="p-1 rounded-lg">08:00</span>
+                            <span className="p-1 rounded-lg">09:00</span>
+                            <span className="p-1 rounded-lg">10:00</span>
+                            <span className="p-1 rounded-lg">11:00</span>
+                            <span className="p-1 rounded-lg">12:00</span>
+                            <span className="p-1 rounded-lg">13:00</span>
+                            <span className="p-1 rounded-lg">14:00</span>
+                            <span className="p-1 rounded-lg">15:00</span>
+                        </div>
+                    </div>
                 </div>
                 <div className="flex flex-col gap-4">
                     <Button >
